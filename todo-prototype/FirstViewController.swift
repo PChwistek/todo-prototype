61adcdd0b2707c8a7716ffffba63bfa5cf553db8
Philip Chwistek <mrsportsnerd@gmail.com>
2019-10-13 19:39:49 -0700

    Initial Commit

---
+++ todo-prototype/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  todo-prototype
+//
+//  Created by Phil on 10/13/19.
+//  Copyright © 2019 Phil. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ todo-prototype/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  todo-prototype
+//
+//  Created by Phil on 10/13/19.
+//  Copyright © 2019 Phil. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
